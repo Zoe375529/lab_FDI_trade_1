f39b823d8802f3e5b9a2d1f0a65e9964b7a0ea7b
Huan <262015110+Zoe375529@users.noreply.github.com>
2026-05-20 18:28:52 -0700

    Add data cleaning and unit conversion

--- lab_exports_top10.R
+++ lab_exports_top10.R
@@ -27,3 +27,23 @@ exports_cty_yr <- getCensus(
 
 # Check what we got
 head(exports_cty_yr)
+
+## Part 2: Clean the data ----
+
+# Filter out region codes and other aggregation codes
+# We only want individual country data, not regional totals
+exports_cty_yr_clean <- exports_cty_yr %>%
+  filter(!(substr(CTY_CODE, 1, 1) == "0" |
+             substr(CTY_CODE, 2, 2) == "X" |
+             substr(CTY_CODE, 1, 1) == "-"))
+
+# Convert ALL_VAL_YR and YEAR to numeric, rescale to billions of dollars
+exports_cty_yr_clean <- exports_cty_yr_clean %>%
+  mutate(ALL_VAL_YR = as.numeric(ALL_VAL_YR) / 1000000000,
+         YEAR = as.numeric(YEAR))
+
+# Check for NAs from conversion (should be 0 or very small)
+sum(is.na(exports_cty_yr_clean$ALL_VAL_YR))
+
+# Preview cleaned data
+head(exports_cty_yr_clean)
